de6d0edfcf67530a8e2c9c2ad67ee94a1eb4e497
Toshio Minei <sminei10@gmail.com>
2026-02-07 01:12:55 -0500

    enable prisma

---
+++ src/lib/prisma.ts
@@ -0,0 +1,25 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+import { PrismaClient } from "../../generated/prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const globalForPrisma = global as typeof globalThis & {
+  prisma?: PrismaClient;
+};
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export { prisma };
